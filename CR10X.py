db149307316e4e0ff93b4ba3891e0fe9e044625a
Tim Horton <hortont424@gmail.com>
2011-05-26 11:04:44 -0400

    when a row fails to parse, throw it out and print a message about it

--- CR10X.py
+++ CR10X.py
@@ -5,9 +5,9 @@ def convert_time(short_form):
     For example, 359 becomes 03:59:00 AM"""
     minutes = short_form[-2:]
     hours = short_form[:-2]
-    
+
     if hours == "": hours = 0
-    
+
     return datetime.time(int(hours), int(minutes)).strftime("%H:%M:%S")
 
 def convert_date(day_of_year, year):
@@ -28,19 +28,19 @@ class CR10X(object):
         """Given a two dimensional array of data, return a new,
         filtered two-dimensional array of data."""
         new_data = []
-        
+
         # Grab the current year from the first "13" record
         self.current_year = [row[12] for row in data if row[0] == "13"][0]
-        
+
         for row in data:
             new_row = self.process_row(row)
-            
+
             # Only re-add the row if process_row returns *something*
             if new_row:
                 new_data.append(new_row)
-            
+
         return new_data
-    
+
     def process_row(self, row):
         """Given a single row of data, translate it into the new format.
         Return None if the row should be dropped in the output table."""
@@ -48,19 +48,23 @@ class CR10X(object):
         if row[0] == "13":
             self.current_year = row[12]
             return None
-        
+
         # If we run into a "9" record, emit a new WISKI record
         if row[0] == "9":
-            # Decompose record into individual values
-            (row_type, day_of_year, timestamp, id_num, day_of_year2,
-             batt_volt_min, air_temp_avg, rel_humid_max, rel_humid_min,
-             avg_rad_avg, avg_par_avg, wind_spd_wvc1, wind_spd_wvc2,
-             wind_spd_wvc3, inten_tot, snow_depth, quality) = row
-            
+            try:
+                # Decompose record into individual values
+                (row_type, day_of_year, timestamp, id_num, day_of_year2,
+                 batt_volt_min, air_temp_avg, rel_humid_max, rel_humid_min,
+                 avg_rad_avg, avg_par_avg, wind_spd_wvc1, wind_spd_wvc2,
+                 wind_spd_wvc3, inten_tot, snow_depth, quality) = row
+            except ValueError:
+                print "    Failed to parse row, throwing it away:\n    {0}".format(",".join(row))
+                return None
+
             # Convert time and date into WISKI formats
             time_string = convert_time(timestamp)
             date_string = convert_date(day_of_year, self.current_year)
-            
+
             # Emit new row, in WISKI format
             return ["DATA", "", "", "", "G9", "S14",
                     date_string,
