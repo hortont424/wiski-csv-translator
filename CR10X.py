3cf90bb162a5efd2a76f05943ad5815f8d20e729
Tim Horton <hortont424@gmail.com>
2011-05-26 11:26:01 -0400

    Fill in zeros for missing columns

--- CR10X.py
+++ CR10X.py
@@ -32,8 +32,8 @@ class CR10X(object):
         # Grab the current year from the first "13" record
         self.current_year = [row[12] for row in data if row[0] == "13"][0]
 
-        for row in data:
-            new_row = self.process_row(row)
+        for index, row in enumerate(data):
+            new_row = self.process_row(row, index)
 
             # Only re-add the row if process_row returns *something*
             if new_row:
@@ -41,7 +41,7 @@ class CR10X(object):
 
         return new_data
 
-    def process_row(self, row):
+    def process_row(self, row, index):
         """Given a single row of data, translate it into the new format.
         Return None if the row should be dropped in the output table."""
         # If we run into another "13" record, update the current year
@@ -51,13 +51,23 @@ class CR10X(object):
 
         # If we run into a "9" record, emit a new WISKI record
         if row[0] == "9":
+            data = {}
+
             try:
                 # Decompose record into individual values
-                (row_type, day_of_year, timestamp, id_num, day_of_year2,
-                 batt_volt_min, air_temp_avg, rel_humid_max, rel_humid_min,
-                 avg_rad_avg, avg_par_avg, wind_spd_wvc1, wind_spd_wvc2,
-                 wind_spd_wvc3, inten_tot, snow_depth, quality) = row
-            except ValueError:
+                # Use 0.0 as the default value if a column is missing
+                (row_type, day_of_year, timestamp) = row[:3]
+                remaining_fields = ["id_num", "day_of_year2", "batt_volt_min", "air_temp_avg",
+                                    "rel_humid_max", "rel_humid_min", "avg_rad_avg", "avg_par_avg",
+                                    "wind_spd_wvc1", "wind_spd_wvc2", "wind_spd_wvc3", "inten_tot",
+                                    "snow_depth", "quality"]
+                for remaining_value in row[3:]:
+                    field_name = remaining_fields.pop(0)
+                    data[field_name] = remaining_value
+                for field_name in remaining_fields:
+                    data[field_name] = "0.0"
+                    print "Row {0} missing column {1}... filling in 0!".format(index, field_name)
+            except:
                 print "    Failed to parse row, throwing it away:\n    {0}".format(",".join(row))
                 return None
 
@@ -69,17 +79,17 @@ class CR10X(object):
             return ["DATA", "", "", "", "G9", "S14",
                     date_string,
                     time_string,
-                    convert_number(id_num),
-                    convert_number(day_of_year),
-                    convert_number(batt_volt_min),
-                    convert_number(air_temp_avg),
-                    convert_number(rel_humid_max),
-                    convert_number(rel_humid_min),
-                    convert_number(avg_rad_avg),
-                    convert_number(avg_par_avg),
-                    convert_number(wind_spd_wvc1),
-                    convert_number(wind_spd_wvc2),
-                    convert_number(wind_spd_wvc3),
-                    convert_number(inten_tot),
-                    convert_number(snow_depth),
-                    convert_number(quality)]
+                    convert_number(data["id_num"]),
+                    convert_number(data["day_of_year2"]),
+                    convert_number(data["batt_volt_min"]),
+                    convert_number(data["air_temp_avg"]),
+                    convert_number(data["rel_humid_max"]),
+                    convert_number(data["rel_humid_min"]),
+                    convert_number(data["avg_rad_avg"]),
+                    convert_number(data["avg_par_avg"]),
+                    convert_number(data["wind_spd_wvc1"]),
+                    convert_number(data["wind_spd_wvc2"]),
+                    convert_number(data["wind_spd_wvc3"]),
+                    convert_number(data["inten_tot"]),
+                    convert_number(data["snow_depth"]),
+                    convert_number(data["quality"])]
